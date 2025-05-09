9c3885308b5f838620a46874c89a434b0a847702
Rodrigo César <rodrigocnn@gmail.com>
2025-05-09 13:08:25 -0300

    feat(routes): add resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+
   resources :cities
   resources :neighborhoods
+  resources :clients
+  resources :owners
+  resources :properties
+  resources :property_types
+  resources :rental_contracts
+  resources :rent_payments
 end
+
