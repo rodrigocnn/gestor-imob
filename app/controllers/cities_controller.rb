479d1eb8c2273a40a21f1c225d7bf14c6195f006
Rodrigo César <rodrigocnn@gmail.com>
2025-05-08 22:25:14 -0300

    feat(cities): add crud methods cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,47 @@
 class CitiesController < ApplicationController
-end
+    before_action :set_city, only: [:show, :update, :destroy]
+  
+    def index
+      @cities = City.all
+      render json: @cities
+    end
+  
+    def show
+      render json: @city
+    end
+  
+    def create
+      @city = City.new(city_params)
+      if @city.save
+        render json: @city, status: :created
+      else
+        render json: { errors: @city.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @city.update(city_params)
+        render json: @city
+      else
+        render json: { errors: @city.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @city.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_city
+      @city = City.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Cidade não encontrada' }, status: :not_found
+    end
+  
+    def city_params
+      params.require(:city).permit(:name)
+    end
+  end
+  
