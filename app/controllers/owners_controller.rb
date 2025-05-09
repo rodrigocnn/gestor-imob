62231adaced674669de21d25709cc29716d985de
Rodrigo César <rodrigocnn@gmail.com>
2025-05-09 13:06:51 -0300

    fix(controllers): fix params - add cpf owners

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,20 +1,17 @@
 # app/controllers/owners_controller.rb
 class OwnersController < ApplicationController
-    # Antes de processar as ações, garante que o owner esteja presente
+
     before_action :set_owner, only: [:show, :update, :destroy]
   
-    # GET /owners
     def index
       @owners = Owner.all
       render json: @owners
     end
   
-    # GET /owners/:id
     def show
       render json: @owner
     end
   
-    # POST /owners
     def create
       @owner = Owner.new(owner_params)
       
@@ -25,7 +22,6 @@ class OwnersController < ApplicationController
       end
     end
   
-    # PATCH/PUT /owners/:id
     def update
       if @owner.update(owner_params)
         render json: @owner
@@ -34,7 +30,6 @@ class OwnersController < ApplicationController
       end
     end
   
-    # DELETE /owners/:id
     def destroy
       @owner.destroy
       head :no_content # HTTP Status 204
@@ -42,14 +37,14 @@ class OwnersController < ApplicationController
   
     private
   
-    # Set the owner based on the ID
+
     def set_owner
       @owner = Owner.find(params[:id])
     end
   
-    # Only allow a trusted parameter "white list" through.
+  
     def owner_params
-      params.require(:owner).permit(:name, :email, :phone)
+      params.require(:owner).permit(:name, :email, :cpf , :phone)
     end
   end
   
