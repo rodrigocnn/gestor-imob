cde75b7f47eb0f4e2eee3db461c86255670f0668
Rodrigo César <rodrigocnn@gmail.com>
2025-05-09 13:09:06 -0300

    feat(db): update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_09_011717) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_09_114629) do
   create_table "cities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "clients", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "cpf"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -25,5 +34,74 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_09_011717) do
     t.index ["city_id"], name: "index_neighborhoods_on_city_id"
   end
 
+  create_table "owners", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "cpf"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "properties", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.string "negotiation_type"
+    t.text "description"
+    t.integer "bedrooms"
+    t.integer "bathrooms"
+    t.integer "suites"
+    t.float "price"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "neighborhood"
+    t.string "state"
+    t.string "address"
+    t.bigint "city_id", null: false
+    t.bigint "property_type_id", null: false
+    t.bigint "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_properties_on_city_id"
+    t.index ["owner_id"], name: "index_properties_on_owner_id"
+    t.index ["property_type_id"], name: "index_properties_on_property_type_id"
+  end
+
+  create_table "property_types", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rent_payments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "rental_contract_id", null: false
+    t.datetime "due_date"
+    t.datetime "payment_date"
+    t.float "amount"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rental_contract_id"], name: "index_rent_payments_on_rental_contract_id"
+  end
+
+  create_table "rental_contracts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "client_id", null: false
+    t.bigint "property_id", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.float "monthly_rent"
+    t.float "deposit_amount"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_rental_contracts_on_client_id"
+    t.index ["property_id"], name: "index_rental_contracts_on_property_id"
+  end
+
   add_foreign_key "neighborhoods", "cities"
+  add_foreign_key "properties", "cities"
+  add_foreign_key "properties", "owners"
+  add_foreign_key "properties", "property_types"
+  add_foreign_key "rent_payments", "rental_contracts"
+  add_foreign_key "rental_contracts", "clients"
+  add_foreign_key "rental_contracts", "properties"
 end
